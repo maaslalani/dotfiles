947b28fcf88690f5ffd9eb18879e22571bf13435
Maas Lalani <maaslalani1@gmail.com>
2019-02-05 16:43:37 -0500

    Remove unnecessary aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -13,10 +13,6 @@ alias got='git '
 alias get='git '
 alias gut='git '
 
-# Folder aliases
-alias projects=~/Documents/Projects
-alias dev1=~/Documents/Shopify/dev1/
-
 # Configuration aliases
 alias themeconfig='vim ~/.oh-my-zsh/custom/themes/powerlevel9k/prompt_powerlevel9k_setup'
 alias zshrc='cd ~/dotfiles/.zsh && vim ~/dotfiles/.zsh'
