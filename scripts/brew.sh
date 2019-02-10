a3f807b4879b6f511085a8196d0d562e210da43a
Maas Lalani <maaslalani1@gmail.com>
2019-02-10 16:28:27 -0500

    Add scripts

---
+++ scripts/brew.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Use the latest version of Homebrew
+brew update
+
+# Upgrade any existing formulae
+brew upgrade
+
+# Install useful binaries
+brew install git
+brew install node
+brew install bat
+brew install tldr
+brew install tree
+brew install vim --with-override-system-vi
+brew install heroku/brew/heroku
+
+# Install casks
+brew cask install iterm2
+brew cask install flux
+brew cask install google-chrome
+brew cask install spotify
+brew cask install slack
+brew cask install docker
+
+# Remove outdated versions from the cellar
+brew cleanup
---
+++ scripts/vim.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+vim +PlugInstall +PlugClean! +qa
