f410f589250704a76a53b2ec4abf2203ed0c4237
Maas Lalani <maaslalani1@gmail.com>
2019-02-28 12:58:51 -0500

    Add tmux configuration

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,3 +17,10 @@ alias gut='git '
 alias themeconfig='vim ~/.oh-my-zsh/custom/themes/powerlevel9k/prompt_powerlevel9k_setup'
 alias zshrc='cd ~/dotfiles/zsh && vim ~/dotfiles/zsh'
 alias vimrc='cd ~/dotfiles/vim && vim ~/dotfiles/vim'
+
+# tmux aliases
+alias tksv='tmux kill-server'
+alias tkss='tmux kill-session -t'
+alias ta='tmux attach -t'
+alias tls='tmux list-sessions'
+alias tn='tmux new-session -s'
