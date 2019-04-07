8d09546e713d66215fc8a2916cd35ff3ab7bdfea
Maas Lalani <maaslalani1@gmail.com>
2019-04-06 20:23:10 -0400

    Remove unnecessary aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,17 +8,6 @@ alias go='git checkout '
 alias gk='gitk --all&'
 alias gx='gitx --all'
 
-# Misspelled git
-alias got='git '
-alias get='git '
-alias gut='git '
-
-# Configuration aliases
-alias themeconfig='vim ~/.oh-my-zsh/custom/themes/powerlevel9k/prompt_powerlevel9k_setup'
-alias zshrc='cd ~/dotfiles/zsh && vim ~/dotfiles/zsh'
-alias vimrc='cd ~/dotfiles/vim && vim ~/dotfiles/vim'
-alias tmuxconfig='cd ~/dotfiles/shell && vim ~/dotfiles/shell/tmux.conf'
-
 # tmux aliases
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
