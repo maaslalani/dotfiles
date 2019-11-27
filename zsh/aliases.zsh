cd5c334675a2b69c0645f0d262e56d8a59fd449c
Maas Lalani <maaslalani1@gmail.com>
2019-11-26 20:15:03 -0500

    boop

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,8 +5,11 @@ alias gb='git branch'
 alias gc='git commit'
 alias gd='git diff'
 alias gd~='git diff HEAD~1'
-alias gk='gitk --all&'
-alias gx='gitx --all'
+alias gco='git checkout'
+alias grim='git rebase -i master'
+alias gp='git push'
+alias gp='git pull'
+alias gpsup='git push --set-upstream origin $(current_branch)'
 
 # tmux aliases
 alias tksv='tmux kill-server'
